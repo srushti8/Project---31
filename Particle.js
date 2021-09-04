c221c0f7fe786d4eeb013c427823133287cdca88
srushti8 <76839929+srushti8@users.noreply.github.com>
2021-09-04 15:25:05 +0530

    Add files via upload

--- Particle.js
+++ Particle.js
@@ -1,8 +1,8 @@
 class Particles {
     constructor(x, y) {
         var options = {
-            restitution: 1,
-            friction: 0,
+            restitution: 0.4
+            //friction: 0,
         }
         this.r = 10;
         this.body = Bodies.circle(x, y, this.r, options);
